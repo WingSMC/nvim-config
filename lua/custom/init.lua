bc4fe8486d3915e0c95d0d050d379f7446bb213a
Gergely Dremak <gergely.dremak@hu.bosch.com>
2024-01-20 10:26:03 +0100

    init

---
+++ lua/custom/init.lua
@@ -0,0 +1,27 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+
+vim.cmd([[
+augroup highlight_yank
+    autocmd!
+    au TextYankPost * silent! lua vim.highlight.on_yank { higroup='IncSearch', timeout=200 }
+augroup END
+]])
+
+vim.keymap.set('s', '<Tab>', 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false
+})
+
+vim.keymap.set('i', '<C-Tab>', 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false
+})
+
+-- vim.g.copilot_no_tab_map = true
+
